99a7ef2c5616148ac62bfbb3345a3e78ef9d3585
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2022-04-01 19:03:44 -0300

    Unmarshal numbers without quotes

--- marshaller/marshal.go
+++ marshaller/marshal.go
@@ -3,9 +3,23 @@ package marshaller
 import (
 	"fmt"
 	"github.com/alancesar/gogram/measure"
+	"reflect"
 )
 
 func Marshal(input measure.Measurable) ([]byte, error) {
+	if isNumeric(input) {
+		return []byte(input.String()), nil
+	}
+
 	formatted := fmt.Sprintf(`"%s"`, input.String())
 	return []byte(formatted), nil
+}
+
+func isNumeric(input measure.Measurable) bool {
+	switch reflect.ValueOf(input).Kind() {
+	case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int32, reflect.Int64:
+		return true
+	default:
+		return false
+	}
 }
