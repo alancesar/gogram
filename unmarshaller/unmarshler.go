8d788cb110ac65dc556a2c6e4ed9cb9c0cd60f69
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2022-04-01 18:27:05 -0300

    Implement unquoteIfQuoted

--- unmarshaller/unmarshler.go
+++ unmarshaller/unmarshler.go
@@ -1,16 +1,38 @@
 package unmarshaller
 
 import (
+	"fmt"
 	"github.com/alancesar/gogram/measure"
-	"strconv"
 )
 
-func Unmarshal[T measure.Measurable](self *T, stringer func(input string) T, bytes []byte) error {
-	raw, err := strconv.Unquote(string(bytes))
+const quotes = '"'
+
+func Unmarshal[T measure.Measurable](self *T, fromString func(input string) T, bytes []byte) error {
+	raw, err := unquoteIfQuoted(string(bytes))
 	if err != nil {
 		return err
 	}
 
-	*self = stringer(raw)
+	*self = fromString(raw)
 	return nil
+}
+
+// Thanks https://github.com/shopspring/decimal
+func unquoteIfQuoted(value interface{}) (string, error) {
+	var bytes []byte
+
+	switch v := value.(type) {
+	case string:
+		bytes = []byte(v)
+	case []byte:
+		bytes = v
+	default:
+		return "", fmt.Errorf("could not convert value '%+v' to byte array of type '%T'", value, value)
+	}
+
+	if len(bytes) > 2 && bytes[0] == quotes && bytes[len(bytes)-1] == quotes {
+		bytes = bytes[1 : len(bytes)-1]
+	}
+
+	return string(bytes), nil
 }
